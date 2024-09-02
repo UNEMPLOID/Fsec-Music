87eed2dc982262d6040ce9ce3041c2f119bc4d9a
CHALCOGEN645 <166417113+CHALCOGEN645@users.noreply.github.com>
2024-09-02 08:49:49 +0530

    Update __main__.py

--- Fsecmusic/__main__.py
+++ Fsecmusic/__main__.py
@@ -36,26 +36,26 @@ async def init():
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("Fsecmusic.plugins" + all_module)
-    LOGGER("Fsecmusic.plugins").info("ғsᴇᴄ's ᴍᴏᴅᴜʟᴇs ʟᴏᴀᴅᴇᴅ...")
+    LOGGER("Fsecmusic.plugins").info("Fsecmusic's ᴍᴏᴅᴜʟᴇs ʟᴏᴀᴅᴇᴅ...")
     await userbot.start()
-    await FALCON.start()
+    await JARVIS.start()
     try:
-        await FALCON.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await JARVIS.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("Fsecmusic").error(
-            "ᴘʟᴇᴀsᴇ ᴛᴜʀɴ ᴏɴ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴏғ ʏᴏᴜʀ ʟᴏɢ ɢʀᴏᴜᴘ\ᴄʜᴀɴɴᴇʟ.\n\nғsᴇᴄ ʙᴏᴛ sᴛᴏᴘᴘᴇᴅ..."
+        LOGGER("ANNIEMUSIC").error(
+            "ᴘʟᴇᴀsᴇ ᴛᴜʀɴ ᴏɴ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴏғ ʏᴏᴜʀ ʟᴏɢ ɢʀᴏᴜᴘ\ᴄʜᴀɴɴᴇʟ.\n\nᴀɴɴɪᴇ ʙᴏᴛ sᴛᴏᴘᴘᴇᴅ..."
         )
         exit()
     except:
         pass
-    await FALCON.decorators()
-    LOGGER("Fsecmusic").info(
-        "\x46\x61\x6c\x63\x6f\x6e\x20\x53\x65\x63\x75\x72\x69\x74\x79\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x2e"
+    await JARVIS.decorators()
+    LOGGER("ANNIEMUSIC").info(
+        "\x41\x6e\x6e\x69\x65\x20\x4d\x75\x73\x69\x63\x20\x52\x6f\x62\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x2e\x2e"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("Fsecmusic").info("sᴛᴏᴘɪɴɢ ғsᴇᴄ ᴍᴜsɪᴄ ʙᴏᴛ ...")
+    LOGGER("Fsecmusic").info("sᴛᴏᴘɪɴɢ Fsecmusic ᴍᴜsɪᴄ ʙᴏᴛ ...")
 
 
 if __name__ == "__main__":
